ba558f04f4d0675393388e198c0ff5e70b34fdbd
joamag <joamag@gmail.com>
2013-07-26 21:37:43 +0100

    removed loading type

--- medium/src/medium_plugin.py
+++ medium/src/medium_plugin.py
@@ -47,7 +47,6 @@ class MediumPlugin(colony.base.system.Plugin):
     description = "The plugin that offers the medium service"
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
-    loading_type = colony.base.system.EAGER_LOADING_TYPE
     platforms = [
         colony.base.system.CPYTHON_ENVIRONMENT
     ]
